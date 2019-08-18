14c854b7a997b6ddfbeb9c15602b34c9700d9bef
xixixi-xi <yuanxixixixi@163.com>
2019-08-18 14:46:17 +0800

    add 1

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+
+        <!--轮播图区域-->
+        <mt-swipe :auto="4000">
+            <mt-swipe-item>1</mt-swipe-item>
+            <mt-swipe-item>2</mt-swipe-item>
+            <mt-swipe-item>3</mt-swipe-item>
+        </mt-swipe>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.mint-swipe {
+    height: 200px;
+
+    .mint-swipe-item {
+        &:nth-child(1) {
+            background-color: #1b6d85;
+        }
+        &:nth-child(2) {
+            background-color: #dd524d;
+        }
+        &:nth-child(3) {
+            background-color: #398439;
+        }
+    }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 // 入口文件
 import Vue from 'vue'
+//1.1导入路由的包
+import VueRouter from 'vue-router'
+//1.2安装路由
+Vue.use(VueRouter)
+
+//2.1导入 vue-resource
+import VueResource from 'vue-resource'
+//2.2安装 vue-resource
+Vue.use(VueResource)
 
 //导入 MUI 样式
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
 //按需导入 Mint-UI 中的组件
-import { Header } from 'mint-ui';
+import { Header, Swipe, SwipeItem } from 'mint-ui';
 Vue.component(Header.name, Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+//1,3导入自己的 router.js 路由模块
+import router from './router.js'
 
 
 //导入App根组件
@@ -14,5 +29,6 @@ import  app from  './App.vue'
 
 var vm = new Vue({
   el: '#app',
-  render: c => c(app)
+  render: c => c(app),
+  router  //1.4挂载路由对象到VM实例上
 })
