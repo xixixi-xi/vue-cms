5f568350d9b23624f4cc7e79d1f8fc61b78a80f6
xixixi-xi <yuanxixixixi@163.com>
2019-08-18 22:13:52 +0800

    add 2

--- src/main.js
+++ src/main.js
@@ -5,10 +5,22 @@ import VueRouter from 'vue-router'
 //1.2安装路由
 Vue.use(VueRouter)
 
+
+//导入格式化时间的插件
+import moment from 'moment'
+//定义全局的过滤器
+Vue.filter('dataFormat', function (dataStr, pattern = "YYYY-MM-DD HH:mm:ss") {
+  return moment(dataStr).format(pattern)
+})
+
+
 //2.1导入 vue-resource
 import VueResource from 'vue-resource'
 //2.2安装 vue-resource
 Vue.use(VueResource)
+// 设置请求的根路径
+//Vue.http.options.root = 'http://vue.studyit.io';
+
 
 //导入 MUI 样式
 import './lib/mui/css/mui.min.css'
